dd739729ed21efa73a36c28d9f72f64780b42393
Michael Collas <mcollas@yahoo.com>
2012-06-05 09:04:33 +1000

    Fixed error in setting writing file size: was double counting size because the @size member of ZipEntry was being intialized to the specified size instead of zero.

--- lib/doo_dah/zip_entry.rb
+++ lib/doo_dah/zip_entry.rb
@@ -6,24 +6,38 @@ module DooDah
     include LocalDirectoryHeader
     include CentralDirectoryHeader
 
-    attr_reader :size, :crc, :name, :closed
+    attr_reader :name, :closed
     alias_method :closed?, :closed
 
     extend ::Forwardable
     def_delegators :@zip_stream, :write, :current_offset
 
-    def initialize(zip_stream, name, size = 0)
+    def initialize(zip_stream, name, size = 0, crc = 0)
       @zip_stream = zip_stream
       @name = name
       @crc = 0
-      @size = size
+      @size = 0
+      @expected_crc = crc
+      @expected_size = size
       @closed = false
       write_local_header
     end
 
+    def size
+      @expected_size != 0 ? @expected_size : @size
+    end
+
+    def crc
+      @expected_crc != 0 ? @expected_crc : @crc
+    end
+
+    def local_footer_required?
+      @expected_size == 0 || @expected_crc == 0
+    end
+
     def close
       return if closed
-      write_local_footer
+      write_local_footer if local_footer_required?
       @closed = true
     end
 
